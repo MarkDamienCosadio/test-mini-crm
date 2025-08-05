6199c212b65082d4710b46c753bfc5ef8a404c93
MarkDamienCosadio <markcosadio@gmail.com>
2025-08-05 09:09:37 +0800

    Update Force-Dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { LeadsTable } from '@/components/leads-table';
 import { AddLeadDialog } from '@/components/add-lead-dialog';
 import { LeadStatus } from '@prisma/client';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const leads = await prisma.lead.findMany({
     orderBy: { createdAt: 'desc' },
